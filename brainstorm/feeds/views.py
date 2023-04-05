f58f0f4a5ddf1e98aa8a2c43c81648859a762140
Marat Khagazheev <marathagazeev@gmail.com>
2023-04-05 21:23:24 +0300

    Hide lines with projects model

--- brainstorm/feeds/views.py
+++ brainstorm/feeds/views.py
@@ -1,26 +1,27 @@
 from django.views.generic import ListView
-from projects.models import Project
+
+# from projects.models import Project
 
 
 class NewProjectsView(ListView):
     template_name = 'feeds/new_projects_feed.html'
     context_object_name = 'projects'
-    queryset = Project.objects.new()
+    # queryset = Project.objects.new()
 
 
 class BestProjectsView(ListView):
     template_name = 'feeds/best_projects_feed.html'
     context_object_name = 'projects'
-    queryset = Project.objects.best()
+    # queryset = Project.objects.best()
 
 
 class SpeakedProjectsView(ListView):
     template_name = 'feeds/speaked_projects_feed.html'
     context_object_name = 'projects'
-    queryset = Project.objects.speaked()
+    # queryset = Project.objects.speaked()
 
 
 class ArchiveProjectsView(ListView):
     template_name = 'feeds/archive_projects_feed.html'
     context_object_name = 'projects'
-    queryset = Project.objects.archive()
+    # queryset = Project.objects.archive()
