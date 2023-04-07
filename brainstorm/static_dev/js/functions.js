51e4ab977d07cacd7f710df57e69664752c1a6b8
Ростислав <shishmarev.r@mail.ru>
2023-04-07 13:21:59 +0300

    Add tags selector, fix flake8 config

---
+++ brainstorm/static_dev/js/functions.js
@@ -0,0 +1,43 @@
+`use strict`
+
+export function normalizeUrl(url) {
+    while (url.includes("??")) {
+        url = url.replace("??", "?");
+    }
+    if (!url.includes("?")) {
+        url += "?";
+    }
+    return url;
+}
+
+export function getUrlWithNewArgument(arg, value, url=window.location.href) {
+    let baseUrl, argsString;
+    [baseUrl, argsString] = normalizeUrl(url).split("?");
+    let newArgs = [];
+    for (let argument of argsString.split("&")) {
+        if (!argument.startsWith(`${arg}=`) && argument) {
+            newArgs.push(argument);
+        }
+    }
+    if (value) {
+        newArgs.push(`${arg}=${value}`);
+    }
+    return `${baseUrl}?${newArgs.join("&")}`
+}
+
+export function getArgumentFromUrl(arg, default_="", url=window.location.href) {
+    let baseUrl, argsString;
+    [baseUrl, argsString] = normalizeUrl(url).split("?");
+    for (let argument of argsString.split("&")) {
+        let param, value;
+        [param, value] = argument.split("=");
+        if (param == arg) {
+            return value;
+        }
+    }
+    return default_;
+}
+
+export function getSelectedValues(select) {
+    return Array.from(select.options).filter(option => option.selected).map(option => option.value);
+}
--- brainstorm/static_dev/js/search_string.js
+++ brainstorm/static_dev/js/search_string.js
@@ -1,30 +1,10 @@
+import {getUrlWithNewArgument, getArgumentFromUrl} from './functions.js';
+
 function redirect() {
     let input = document.querySelector("#search-string > input");
-    let current_url = window.location.href;
-    let new_url;
-    if (!current_url.includes("?")) {
-        new_url = current_url + "?search=" + input.value;
-    } else {
-        let args_string;
-        while (current_url.includes("??")) {
-            current_url = current_url.replace("??", "?");
-        }
-        [new_url, args_string] = current_url.split("?");
-        let new_args = [];
-        for (let arg_string of args_string.split("&")) {
-            let param, value;
-            [param, value] = arg_string.split("=")
-            if (param != "search") {
-                new_args.push(arg_string);
-            }
-        }
-        if (input.value) {
-            new_args.push("search=" + input.value);
-        }
-        new_url += "?" + new_args.join("&");
-    }
-    console.log(input.value, new_url);
+    let new_url = getUrlWithNewArgument("search", input.value);
     window.location.replace(new_url);
 }
 
 document.querySelector("#search-string > button").onclick = redirect;
+document.querySelector("#search-string > input").value = getArgumentFromUrl("search", "")
