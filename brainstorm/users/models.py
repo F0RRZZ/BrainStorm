57288f81324fe83188e10ef7aba3160a020ef8a6
Marat Khagazheev <marathagazeev@gmail.com>
2023-04-09 19:54:21 +0300

    Add uniqueness to the username

--- brainstorm/users/models.py
+++ brainstorm/users/models.py
@@ -43,6 +43,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         'username',
         max_length=100,
         help_text='Имя пользователя',
+        unique=True,
     )
     bio = models.TextField(
         'bio',
