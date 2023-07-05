7ba8119292347ea3203d1850de22144fd5539091
marat <marathagazeev@gmail.com>
2023-07-06 00:20:05 +0300

    Add api permissions

--- brainstorm/api/v1/users/views.py
+++ brainstorm/api/v1/users/views.py
@@ -1,9 +1,15 @@
 import rest_framework.viewsets
 
+import api.v1.users.permissions
 import api.v1.users.serializers
+import core.api.permissions
 import users.models
 
 
 class UserViewSet(rest_framework.viewsets.ModelViewSet):
     queryset = users.models.User.objects.all()
     serializer_class = api.v1.users.serializers.UserSerializer
+    permission_classes = (
+        core.api.permissions.IsAdminOrReadOnly,
+        api.v1.users.permissions.IsOwnerOrReadOnly,
+    )
