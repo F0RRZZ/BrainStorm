6ca6d329042c428362bda0c25a6af25af136095a
marat <marathagazeev@gmail.com>
2023-07-05 19:32:06 +0300

    Add routers

--- brainstorm/api/v1/projects/routers.py
+++ brainstorm/api/v1/projects/routers.py
@@ -2,6 +2,5 @@ import rest_framework.routers
 
 import api.v1.projects.views
 
-
 router = rest_framework.routers.SimpleRouter()
 router.register(r'projectlist', api.v1.projects.views.ProjectViewSet)
